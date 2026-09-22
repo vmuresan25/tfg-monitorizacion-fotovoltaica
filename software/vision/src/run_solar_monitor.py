d44cbe57475c77e621e4ea30bc33eccd897fd293
vmuresan25 <vmuresan25@gmail.com>
2026-09-22 20:24:25 +0200

    Preserve virtual environment Python path (#4)

--- software/vision/src/run_solar_monitor.py
+++ software/vision/src/run_solar_monitor.py
@@ -21,7 +21,7 @@ DAILY_SUMMARY_DIR = BASE_DIR / "daily_summary"
 
 ESP32_URL = "http://192.168.1.145/capture"
 
-PYTHON_PATH = str(Path(sys.executable).resolve())
+PYTHON_PATH = os.path.abspath(sys.executable)
 
 
 #SOL
